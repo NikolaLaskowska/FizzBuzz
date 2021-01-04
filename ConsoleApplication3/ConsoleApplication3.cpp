60e571e88ce66cb6d4883e683a9308b38205f2e4
Nikola Laskowska <71625535+NikolaLaskowska@users.noreply.github.com>
2021-01-04 14:32:35 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,27 @@
+﻿/*
+Common Interwiev Task: "FizzBuzz"
+NikolaLaskowska 2019
+*/
+
+#include <iostream>
+
+int main()
+{
+    int i=1;
+    for (i; i <= 100; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            std::cout << "FizzBuzz";
+        }
+        else if (i % 3 == 0) {
+            std::cout << "Fizz";
+        }
+        else if (i % 5 == 0) {
+            std::cout << "Buzz";
+        }
+        else {
+            std::cout << i;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
